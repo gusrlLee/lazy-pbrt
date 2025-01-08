0b406ee66401a9f1a39fa83fcc25ee6db6c3c3fd
gusrlLee <gusrl3204@naver.com>
2025-01-08 15:48:37 +0900

    delete old folder

--- old/color.h
+++
@@ -1,7 +0,0 @@
-#pragma once 
-#include "vec3.h"
-#include <iostream>
-
-using Color = Vec3;
-
-void write_color(std::ostream &out, Color pixel_color);
--- old/interval.cpp
+++
@@ -1,4 +0,0 @@
-#include "interval.h"
-
-const Interval Interval::empty      = Interval(+inf, -inf);
-const Interval Interval::universe   = Interval(-inf, +inf);
